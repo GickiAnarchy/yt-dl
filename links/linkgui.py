c77d041bfe97e822b6a6f07881e6b0eec4561b57
GickiAnarchy <mindofmagick85@gmail.com>
2022-07-16 04:28:19 -0500

    Save file persistance

--- links/linkgui.py
+++ links/linkgui.py
@@ -25,16 +25,19 @@ BKP = f"{data_dir}/.links.fa.bak"
 
 class LinkGui:
     def __init__(self, linklist = None):
+        # Using sg.Print when print() is used to show the terminal output in thr GUI
         print = sg.Print
-        if not os.path.isdir(data_dir):
-            os.makedirs(data_dir)
-        if not os.path.exists(COMPLETED):
-            self.save_completed(create = True)
+        #Directory Creation
+        if not os.path.isdir(data_dir): os.makedirs(data_dir)
+        if not os.path.isdir(f"{data_dir}/Videos"): os.makedirs(f"{data_dir}/Videos")
+        if not os.path.isdir(f"{data_dir}/Audio"): os.makedirs(f"{data_dir}/Audio")
+        #LinkList Handling
         if linklist == None:
             self.LL = LinkList()
         else:
             self.LL = linklist
-        self.all_completed = []
+        #Save file name, if loaded or saved in session. (To avoid a popup constantly)
+        self.saveFile = None
 
     @property
     def LL(self):
@@ -45,50 +48,29 @@ class LinkGui:
 
     @property
     def hasSaved(self):
-        return not os.path.exists(f"{data_dir}/.links.fa")
+        return os.path.exists(f"{data_dir}/.links.fa")
 
     def save(self):
-        fname = pops.pop_save()
-        if fname == False:
+        if self.saveFile == None:
+            pick = pops.pop_save()
+        if pick == False:
             return
-        with open(f"{data_dir}/{fname}", "wb") as file:
+        if pick != False:
+            self.saveFile = pick
+        with open(f"{self.saveFile}", "wb") as file:
             pickle.dump(self.LL, file)
             file.close()
         sg.popup("SAVED")
 
     def load(self):
-        f = pops.pop_load()
-        if f == False:
+        pick = pops.pop_load()
+        if pick == False:
             return
-        with open(f"{data_dir}/{f}", "rb") as file:
+        if pick != False:
+            self.saveFile = pick
+        with open(f"{self.saveFile}", "rb") as file:
             self.LL = pickle.load(file)
             file.close()
-
-    def save_completed(self, create = False):
-        if create:
-            with open(COMPLETED, "w") as f:
-                f.close()
-        self.load_completed()
-        with open(COMPLETED, "w") as file:
-            for com in self.all_completed:
-                comp = f"{com}\n"
-                file.write(comp)
-            file.close()
-
-    def load_completed(self):
-        load_com = []
-        with open(COMPLETED, "r") as file:
-            for line in file:
-                ln = line[:-1]
-                load_com.append(ln)
-            file.close()
-            for f in load_com:
-                if f not in self.all_completed:
-                    self.all_completed.append(ln)
-                    print(f"ADDED TO ALL COMPLETED LIST:\n{ln}")
-
-    def merge_completed(self):
-        pass
 
     def add(self, url: str):
         if url == "MAGICK":
@@ -111,9 +93,30 @@ class LinkGui:
         perfect = True
         for link in self.LL.current:
             yt = YouTube(link)
-            #video = yt.streams.filter(only_audio=True).first()
-            video = yt.streams.get_audio_only()
-            destination = data_dir
+            audio = yt.streams.get_audio_only()
+            destination = f"{data_dir}/Audio"
+            try:
+                out_file = audio.download(output_path=destination)
+            except:
+                err_link = f"::ERROR::\n{link}"
+                self.LL.current.remove(link)
+                self.LL.current.append(err_link)
+                perfect = False
+                continue
+            base, ext = os.path.splitext(out_file)
+            print(ext)
+            new_file = base + '.mp3'
+            os.rename(out_file, new_file)
+            name = os.path.split(new_file)
+            self.LL.add_com(link, name[1])
+        return perfect
+
+    def download_video(self):
+        perfect = True
+        for link in self.LL.current:
+            yt = YouTube(link)
+            video = yt.streams.first()
+            destination = f"{data_dir}/Videos"
             try:
                 out_file = video.download(output_path=destination)
             except:
@@ -124,7 +127,7 @@ class LinkGui:
                 continue
             base, ext = os.path.splitext(out_file)
             print(ext)
-            new_file = base + '.mp3'
+            new_file = base + '.mp4'
             os.rename(out_file, new_file)
             name = os.path.split(new_file)
             self.LL.add_com(link, name[1])
@@ -140,11 +143,11 @@ class LinkGui:
 
         layout_left = [
         [sg.Text("Enter a link: "), sg.Input(key = "-LINK-")],
+        [sg.Radio("Audio", "AV", default = True, size = (10,1), key = "-RA-"), sg.Radio("Video", "AV", default = False, size = (10,1), key = "-RV-")],
         [sg.Multiline("", size = (50, 10), key = "-console-", reroute_cprint=True, reroute_stdout=True, autoscroll = True, disabled = True)]
         ]
         
         list_size = (50,10)
-
         layout_right = [
         [sg.TabGroup([
         [sg.Tab("Current", [[sg.Listbox(self.LL.current, size = list_size, enable_events = True, key = "-LIST-")]]),
@@ -159,7 +162,7 @@ class LinkGui:
         layout = [
         [sg.Column(layout_left, justification = "left", pad = (5,5)), sg.Column(layout_right, justification = "right", pad = (5,5))],
         [sg.Column(layout_bottom, element_justification = "center", justification = "center", pad = (5,5))],
-        [sg.StatusBar("", key = "-STAT1-", size = (50,3)), sg.StatusBar("", key = "-STAT2-", size = (50,3))]
+        [sg.StatusBar("", key = "-STAT1-", size = (50,2)), sg.StatusBar("", key = "-STAT2-", size = (50,2))]
         ]
         return layout
 
@@ -173,6 +176,7 @@ class LinkGui:
         window = self.make_window()
         while True:
             event, values = window.read()
+
             run_btn = window["-RUN-"]
             load_btn = window["-LOAD-"]
             list_box = window["-LIST-"]
@@ -197,10 +201,13 @@ class LinkGui:
 
             if event == "-CLEAR-":
                 print("Clear pressed")
-                choice = sg.popup_yes_no("Clear the current link list?")
-                if choice == "Yes":
-                    self.LL.current.clear()
-                    print("Link List is cleared")
+                if self.saveFile != None:
+                    choice = sg.popup_yes_no("Clear the current LinkList file?\n(*File will be saved*)")
+                    if choice == "Yes":
+                        self.save()
+                        self.LL == LinkList()
+                        self.saveFile == None
+                        print("Link List is cleared")
 
             if event == "-ADD-":
                 print("Add pressed")
@@ -212,19 +219,26 @@ class LinkGui:
                 if len(self.LL.current) <= 0:
                     sg.popup("HEY! Link List is EMPTY!")
                 run_btn.update(disabled = True)
-                window.perform_long_operation(lambda: self.download_audio(), "-END-")
+                if values["-RA-"] == True:
+                    window.perform_long_operation(self.download_audio(), "-END-")
+                if values["-RV-"] == True:
+                    window.perform_long_operation(self.download_video(), "-END-")
                 run_btn.update(disabled = False)
-                self.save_completed()
 
             if event == "-END-":
                 end = values[event]
                 if end == False:
                     pops.pop_oops()
+                if end == True:
+                    self.save()
 
             if event is not None:
-                load_btn.update(disabled = self.hasSaved)
                 list_box.update(self.LL.current)
                 com_box.update(self.LL.completedNames())
+                if self.saveFile != None:
+                    stat2.update(f"LinkList: {os.path.split(self.saveFile)[1]}")
+                if self.saveFile == None:
+                    stat2.update("No current LinkList file")
                 print("....update....")
 
 
