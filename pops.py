5380b90218dd1d1abeb9a4848b46e3ec7eea6eb9
GickiAnarchy <mindofmagick85@gmail.com>
2022-07-17 19:59:58 -0500

    tagger start

--- pops.py
+++ pops.py
@@ -26,3 +26,13 @@ def splash():
     layout = [[sg.Image(data = base64images.DWY_LOGO)]]
     win = sg.Window("Down With YT", layout, finalize = True, modal = True, alpha_channel = 1, no_titlebar=True, keep_on_top=True, auto_close = True, auto_close_duration = 5)
     return win
+
+
+layout_tagger = [[sg.FileBrowse()]]
+
+def open_tagger():
+    tagger = sg.Window("CArL", layout_tagger, finalize = True, modal = True, alpha_channel = 1, no_titlebar=True)
+    while True:
+        event, values = tagger.read()
+        if event == sg.WIN_CLOSED:
+            tagger.close()
